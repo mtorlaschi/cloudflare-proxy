b548411f255cacde8b76a93392747e8ab55d0040
mtorlaschi <60666317+mtorlaschi@users.noreply.github.com>
2025-06-08 12:17:13 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
-import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   const url = req.query.url;
   if (!url) return res.status(400).send('Missing url parameter');
-  
+
   try {
     const response = await fetch(url, {
       headers: {
@@ -16,4 +16,4 @@ export default async function handler(req, res) {
   } catch (e) {
     res.status(500).send('Error fetching URL');
   }
-}
+};
